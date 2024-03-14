2b8bb0bdcc2e0fa7ec98edace54a680fb24f3acf
GetPsyched6 <roshin006@gmail.com>
2024-03-14 22:47:28 +0530

    Update block.py

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -1,19 +1,27 @@
-from utils.crypto import hash_data
+from ..utils.crypto import hash_data
 import time
 
 
 class Block:
-    def __init__(self, index, transactions, prev_hash):
+    """ """
+
+    def __init__(self, index, transactions, prev_hash, _test_timestamp=None):
         self.index = index
-        self.timestamp = time.time()
+        self._timestamp = (
+            _test_timestamp if _test_timestamp is not None else time.time()
+        )
         self.transactions = transactions
         self.prev_hash = prev_hash
         self.nonce = 0
 
+    @property
+    def timestamp(self):
+        return self._timestamp
+
     def compute_hash(self):
-        block_string = self.__str__()
+        block_string = str(self)
         return hash_data(block_string)
 
     def __str__(self):
-        transactions_string = str([str(tx) for tx in self.transactions])
-        return f"{self.index}{self.timestamp}{self.transactions_string}{self.prev_hash}{self.nonce}"
+        transactions_string = "".join([str(tx) for tx in self.transactions])
+        return f"{self.index}{self._timestamp}{transactions_string}{self.prev_hash}{self.nonce}"
